1ef0c58873c90a96a3b33c1e6421a7f4307c4c52
kennythepark <parkubc@gmail.com>
2019-01-26 21:20:47 -0800

    Implement barcode scan and taking pictures

--- react-native-expo/screens/CameraScreen.js
+++ react-native-expo/screens/CameraScreen.js
@@ -1,56 +1,93 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
+import { BarCodeScanner, Camera, Permissions } from 'expo';
 
 export default class CameraExample extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-  };
+    barcodeTypes = {
+        barCodeTypes: [ BarCodeScanner.Constants.BarCodeType.ean13 ]
+    };
 
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
+    state = {
+        hasCameraPermission: null,
+        barcodeScanning: true,
+        type: Camera.Constants.Type.back,
+    };
 
-  render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View />;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row'
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
     }
-  }
+
+    _toggleBarcodeScanning = () => this.setState({ barcodeScanning: !this.state.barcodeScanning });
+
+    _getBarcodeTypes = {
+        barCodeTypes: [ BarCodeScanner.Constants.BarCodeType.ean13 ]
+    }
+
+    _takePicture = () => {
+        if (this.camera) {
+            console.log('Take picture pressed');
+            this.camera.takePictureAsync({ onPictureSaved: this._onPictureSaved });
+        }
+    };
+
+    _onPictureSaved = async photo => {
+        console.log(photo.uri);
+
+        // await FileSystem.moveAsync({
+        //   from: photo.uri,
+        //   to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+        // });
+    }
+
+    _handleBarCodeRead = data => {
+        this.setState({ barcodeScanning: !this.state.barcodeScanning });
+        Alert.alert(
+            'Scan successful!',
+            JSON.stringify(data)
+        );
+    };
+
+    render() {
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return <View />;
+        } else if (hasCameraPermission === false) {
+            return <Text>No access to camera</Text>;
+        } else {
+            return (
+                <View style={{ flex: 1 }} onLayout={this._test}>
+                    <Camera
+                        ref={ref => {
+                            this.camera = ref;
+                        }}
+                        style={{ flex: 1 }}
+                        type={this.state.type}
+                        barCodeScannerSettings={ this.barcodeTypes }
+                        onBarCodeScanned={ this._handleBarCodeRead }
+                    >
+                        <View
+                            style={{
+                                flex: 1,
+                                backgroundColor: 'transparent',
+                                flexDirection: 'row',
+                            }}>
+                            <TouchableOpacity
+                                style={{
+                                    flex: 1,
+                                    alignSelf: 'flex-end',
+                                    alignItems: 'center',
+                                }}
+                                onPress={ this._takePicture }>
+                                <Text
+                                    style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                                    {' '}Take Picture{' '}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
+            );
+        }
+    }
 }
